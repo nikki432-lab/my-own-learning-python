dc8d98baf0df4cd3dacf58049db077a2b729e031
nikki/dap.python <nikhilgouda432@Gmail.com>
2025-03-28 19:33:34 +0530

    Update swapoftwovaiables.py

--- swapoftwovaiables.py
+++ swapoftwovaiables.py
@@ -1,5 +1,4 @@
-Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+swap of two variables without using third variable
 >>> a =10
 >>> b= 5
 >>> a=a+b
