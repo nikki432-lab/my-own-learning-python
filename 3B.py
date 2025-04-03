21b40e4315eec3eef7a5c83ff4e2b9ac41d47960
nikki/dap.python <nikhilgouda432@Gmail.com>
2025-04-03 22:21:35 +0530

    Add files via upload

---
+++ 3B.py
@@ -0,0 +1,39 @@
+first_name = "Nikhil"
+last_name = "Gouda"
+full_name = first_name + " " + last_name
+print(full_name)
+
+message = "This is a Warning! "
+
+
+
+print(message.upper())
+print(message.lower())
+print(message.strip())
+print(message.replace("Warning", "Error"))
+
+#python string methods. W3 schools...
+
+ 
+print(message)
+print(len(message))
+
+
+name = "Nikhil"
+print(name[4])#index = position - 1, position = index + 1
+print(name[1:5])
+print(name[:6])
+print(name[1:])
+print(name[4])
+print(name[-2])
+print(name[::3])
+
+s = "Nikhil is a \n good boy"
+print (s)
+s = "Nikhil is a \\good boy"
+print (s)
+
+
+
+
+
