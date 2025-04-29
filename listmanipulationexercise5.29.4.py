c50956d3d6f6266b261c1887fe2cb036400b693a
nikki/dap.python <nikhilgouda432@Gmail.com>
2025-04-29 16:10:54 +0530

    Add files via upload

---
+++ listmanipulationexercise5.29.4.py
@@ -0,0 +1,11 @@
+my_list = [10,20,30,40,50]
+print("Initial list:", my_list)
+
+my_list.append(60)
+print("After appending 60:",my_list)
+
+my_list.insert(1,15)
+print("After inserting 15 at second position:",my_list)
+
+del my_list[2]
+print("After removing third item:",my_list)
---
+++ reverseandsortalist5.pylists.py
@@ -0,0 +1,10 @@
+numbers = [5,12,3,8,7]
+
+numbers.sort(reverse=True)
+print("sorted numbers in descending order:", numbers)
+
+numbers.reverse()
+print("Reversed list:",numbers)
+
+numbers.sort()
+print("sorted numbers in ascending order:",numbers)
